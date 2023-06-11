2d1b71ea1c5da4a25f575948c0e4107d5df5c5d2
cocoa-dev-1 <hyeonseung1006@gmail.com>
2023-06-11 21:29:24 +0900

    feat: add lyrics entity to song module

--- src/songs/songs.module.ts
+++ src/songs/songs.module.ts
@@ -4,9 +4,10 @@ import { SongsService } from './songs.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ArtistModule } from '../artist/artist.module';
 import { SongEntity } from 'src/core/entitys/main/song.entity';
+import { LyricsEntity } from 'src/core/entitys/main/lyrics.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([SongEntity]), ArtistModule],
+  imports: [TypeOrmModule.forFeature([SongEntity, LyricsEntity]), ArtistModule],
   controllers: [SongsController],
   providers: [SongsService],
   exports: [SongsService],
