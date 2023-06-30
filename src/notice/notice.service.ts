0ce3611dd6e5a35c841a8aa8395ee147caa73368
cocoa-dev-1 <hyeonseung1006@gmail.com>
2023-06-30 17:12:46 +0900

    fix: fix notice all endpoint

--- src/notice/notice.service.ts
+++ src/notice/notice.service.ts
@@ -12,7 +12,11 @@ export class NoticeService {
   ) {}
 
   async findAll(): Promise<Array<NoticeEntity>> {
+    const currentDate = moment().valueOf();
     return await this.noticeRepository.find({
+      where: {
+        startAt: LessThanOrEqual(currentDate),
+      },
       relations: {
         category: true,
       },
